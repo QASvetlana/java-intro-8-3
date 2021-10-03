8b89482dd0639e26174351505b2580bd5da19b87
den <dengarbuzov@mail.ru>
2021-10-03 23:41:18 +0300

    Perrier and Crystal added

--- src/main/java/qa/andreeva/Main.java
+++ src/main/java/qa/andreeva/Main.java
@@ -4,13 +4,20 @@ public class Main  {
     public static  void main(String[] args) {
        Water newwater = new Water("Bonaqua", 50);
         Water newwater1 = new Water("Aquaminerale", 60);
-
+        Water newwater2 = new Water("Perrier", 120);
+        Water newwater3 = new Water("Crystal", 160);
 
         newwater.byewater("Bonaqua");
         System.out.println("В продаже есть вода:" + newwater.brande + " ее стоимость в рублях:" + newwater.price);
 
         newwater1.byewater("Aquaminerale");
         System.out.println("В продаже есть вода:" + newwater1.brande + " ее стоимость в рублях:" + newwater1.price);
+
+        newwater2.byewater("Perrier");
+        System.out.println("В продаже есть вода:" + newwater2.brande + " ее стоимость в рублях:" + newwater2.price);
+
+        newwater2.byewater("Crystal");
+        System.out.println("В продаже есть вода:" + newwater3.brande + " ее стоимость в рублях:" + newwater3.price);
 
     }
 
